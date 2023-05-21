d04cbe87b5ec6b2428164a05d72722bd27699e9d
Arturo Simon <artysimon@gmail.com>
2023-05-20 21:42:15 -0400

    Add frontendmentor files and attribution

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -5,7 +5,6 @@ const Attribution = () => {
         <footer className={styles.attribution}>
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">Frontend Mentor</a>.
             Coded by <a href="https://www.frontendmentor.io/profile/artimys" target="_blank"  rel="noopener noreferrer">Arty Simon</a>.
-            <span>Hello World { import.meta.env.VITE_API_URL }</span>;
         </footer>
     );
 };
